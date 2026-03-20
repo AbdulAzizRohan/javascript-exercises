3bad50643748c57a9542328afe3b010a3fd632cf
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2026-03-20 16:11:09 +0600

    Solved fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  index = +index;
 
+  if (index < 0) return "OOPS";
+
+  let first = 0;
+  let second = 1;
+
+  if (index === 0) {
+    return first;
+  }
+  if (index === 1) {
+    return second;
+  }
+
+  let fibo = 0;
+
+  for (let i = 2; i <= index; i++) {
+    fibo = first + second;
+    first = second;
+    second = fibo;
+  }
+
+  return fibo;
 };
 
 // Do not edit below this line
