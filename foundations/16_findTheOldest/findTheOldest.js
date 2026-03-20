9ade4dca59230b2f6d488b67c0a2c306e57a39a8
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2026-03-20 21:42:52 +0600

    solved `findTheOldest`

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (peoples) {
+  const thisYear = new Date().getFullYear();
 
+  return peoples.reduce((people, current) => {
+    if (!current.yearOfDeath) {
+      current.yearOfDeath = thisYear;
+    }
+
+    if (!people.yearOfDeath) {
+      people.yearOfDeath = thisYear;
+    }
+
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    const peopleAge = people.yearOfDeath - people.yearOfBirth;
+
+    return currentAge > peopleAge ? current : people;
+  });
 };
 
 // Do not edit below this line
